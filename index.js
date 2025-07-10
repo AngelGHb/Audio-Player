01a5e4ada61b448cca176cde458269cfb95a081c
Angel Solano <angels31927@gmail.com>
2025-07-10 14:28:59 -0400

    Changing to EJS

---
+++ index.js
@@ -0,0 +1,19 @@
+    import express from "express";
+    import bodyParser from "body-parser";
+
+    const port = 3000
+    const app = express();
+
+    app.set('view engine', 'ejs');
+    app.set('views', './views'); // or use an absolute path if needed
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.static("public"));
+
+    app.get('/', (req,res) => {
+        res.render('index.ejs')
+    })
+
+    app.listen(port, () => {
+        console.log(`Server started on localhost: ${port}`)
+    })
